59fd634d0e48f2538a5ae1e74ee3e9c96ad40115
Dann Blair <dann@bitspacedevelopment.com>
2015-09-21 23:39:02 -0500

    getting the id from the atts

--- pano.php
+++ pano.php
@@ -3,11 +3,8 @@
 // The function that actually handles replacing the short code
 function ppm_handler($atts) {
 
-    print_r($atts);
-    die();
-
     $pano = "<style>#ppm_viewer{width: 100% !important; height: 100% !important;}</style>";
-    $pano .= "<iframe id='ppm_viewer' src='http://panopla.com/pano/";
+    $pano .= "<iframe id='ppm_viewer' src='http://panopla.com/pano/" . $atts->pano;
     $pano .= "'></iframe>";
 
     return $pano;
