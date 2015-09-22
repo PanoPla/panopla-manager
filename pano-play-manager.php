ab6ccbd0fab7f82b7cd92ab646c84f6080522483
Dann Blair <dann@bitspacedevelopment.com>
2015-09-21 23:23:38 -0500

    chaging the order of the scripts

--- pano-play-manager.php
+++ pano-play-manager.php
@@ -9,11 +9,11 @@ Author: PanoPla
 Author URI: http://www.panoplay.net
 */
 
+require_once("install.php");
+require_once("pano.php");
+
 // Shortcodes
 add_shortcode("ppm", "ppm_handler");
 
 // Activiation Hook
-// register_activation_hook( __FILE__, 'ppm_install' );
-
-require_once("install.php");
-require_once("pano.php");
+register_activation_hook( __FILE__, 'ppm_install' );
