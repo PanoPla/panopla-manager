47c7bf7a431153e9651241e47c7de5c06d1ec650
Dann Blair <dann@bitspacedevelopment.com>
2015-09-21 23:44:33 -0500

    Trying to work with the atts

--- pano.php
+++ pano.php
@@ -1,10 +1,17 @@
 <?php
 
 // The function that actually handles replacing the short code
-function ppm_handler($atts) {
+function ppm_handler($atts = array(), $out = FALSE) {
+
+    static $args = array(
+      'pano' => '1'
+    );
+
+    if ( $out ) return $args;
+    $args = shortcode_atts( $args, $atts, 'ppm' );
 
     $pano = "<style>#ppm_viewer{width: 100% !important; height: 100% !important;}</style>";
-    $pano .= "<iframe id='ppm_viewer' src='http://panopla.com/pano/" . $atts->pano;
+    $pano .= "<iframe id='ppm_viewer' src='http://panopla.com/pano/" . $args['pano'];
     $pano .= "'></iframe>";
 
     return $pano;
