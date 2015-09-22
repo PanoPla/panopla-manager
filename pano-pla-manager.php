6b7a62d35719e544ecba5dcbb238d26bc09178f5
Dann Blair <dann@bitspacedevelopment.com>
2015-09-22 00:02:43 -0500

    Refactoring names

---
+++ pano-pla-manager.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Plugin Name: PanoPlaManager
+Plugin URI: http://wordpress.org/plugins/PanoPlaManager/
+Description: Embed a pano on your site
+Version: 0.1
+Author: PanoPla
+Author URI: http://www.panoplay.net
+*/
+
+require_once("ppminstall.php");
+require_once("pano.php");
+
+// Shortcodes
+add_shortcode("ppm", "ppm_handler");
+
+// Activiation Hook
+// register_activation_hook( __FILE__, 'ppm_install' );
+// There is nothing being stored in the database right now
